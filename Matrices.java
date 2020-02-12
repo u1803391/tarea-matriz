0574347a64252c74d81da06298a71efef1280111
u1803391 <60480527+u1803391@users.noreply.github.com>
2020-02-12 18:42:51 -0500

    Add files via upload

---
+++ Matrices.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matrices;
+import java.util.Scanner;
+public class Matrices {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Scanner entrada = new Scanner (System.in);
+        
+       double v[][];
+       int fila;
+       int columna;
+       double sumamatrices= 0;
+       
+       
+       System.out.println("Ingrese la cantidad de filas: ");
+       fila = entrada.nextInt();
+       
+       System.out.println("Ingrese la cantidad de columnas: ");
+       columna = entrada.nextInt();
+       
+       v= new double[fila][columna];
+               
+       for(int i=0;i<fila;i++)
+       {
+           for(int j=0;j<columna;j++)
+           {
+               System.out.println("Ingrese el valor para el espacio ["+i+"]["+j+"]: ");
+               v[i][j] = entrada.nextDouble();
+           }
+       }
+       System.out.println("\nLa matriz es: ");
+       for(int i=0;i<fila;i++)
+       {
+           for(int j=0;j<columna;j++)
+           {
+               System.out.print(v[i][j]+"  ");
+           }
+           System.out.println("\n ");
+       } 
+       
+       
+        for(int i=0;i<fila;i++)
+       {
+           for(int j=0;j<columna;j++)
+           {
+               if(i == j)
+               {
+               sumamatrices = sumamatrices + v[i][j];
+               }
+               if(i + j == fila - 1 )
+               {
+                   sumamatrices = sumamatrices + v[i][j];
+               }
+           }
+           System.out.println("\n ");
+       } 
+       
+        System.out.println("La suma de las diagonales es: "+sumamatrices);
+       
+    }
+    
+}
